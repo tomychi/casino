b96321b4d3656f3df7c586a90a4c82dbb55031c7
tomychi <tomychi352@gmail.com>
2025-10-02 00:50:00 -0300

    a

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,5 @@ import tailwindcss from '@tailwindcss/vite';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: '/casino/', // Cambiá esto por el nombre exacto de tu repo
 });
